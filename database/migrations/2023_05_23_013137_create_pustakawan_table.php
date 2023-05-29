6ebd68c8820a183321c5321c773ecf691b3cacc3
m1tsuha4 <114287774+m1tsuha4@users.noreply.github.com>
2023-05-29 21:21:42 +0700

    V1

---
+++ database/migrations/2023_05_23_013137_create_pustakawan_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pustakawan', function (Blueprint $table) {
+            $table->id('id_pustakawan');
+            $table->string('nama_pustakawan');
+            $table->date('tgl_lahir_pustakawan');
+            $table->string('alamat_pustakawan');
+            $table->string('tlp_pustakawan');
+            $table->string('email_pustakawan')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pustakawan');
+    }
+};
